d1bfca3d23b5a734c63e7c9343bc23c2aaa74933
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-04-05 13:22:56 +0100

    completed extensions

---
+++ petshop/pet_shop_functions.js
@@ -0,0 +1,127 @@
+myFunctions = {
+  // Write your functions here
+  getName(petShop){
+    /*TEST1*/
+    return petShop.name;
+  },
+  getTotalCash(petShop){
+    /*TEST2*/
+    return petShop.admin.totalCash;
+  },
+  addOrRemoveCash(petShop,cash){
+    /*TEST3*/ /*TEST4*/
+    petShop.admin.totalCash += cash;
+  },
+  getPetsSold(petShop){
+    /*TEST5*/
+    return petShop.admin.petsSold;
+  },
+  increasePetsSold(petShop,numberSold){
+    /*TEST6*/
+    petShop.admin.petsSold += numberSold;
+  },
+  getStockCount(petShop){
+    /*TEST7*/
+    return petShop.pets.length;
+  },
+  getPetsByBreed(petShop, breed){
+    /*TEST8*//*TEST9*/
+    let petsOfBreed = [];
+    for (let pet of petShop.pets) {
+      if (pet.breed == breed) {
+        petsOfBreed.push(pet)
+      };
+    };
+    return petsOfBreed.length;
+  },
+  getPetByName(petShop, petName){
+    /*TEST10*/ /*TEST11*/
+    for (let pet of petShop.pets) {
+      if(pet.name == petName){
+        return pet;
+      };
+    }
+    return undefined;
+  },
+  removePetByName(petShop, petName){
+    /*TEST12*/
+    for (let i = petShop.pets.length-1; i >= 0; i--){
+      if (petShop.pets[i].name == petName){
+        petShop.pets.splice(i,1);
+      };
+    };
+  },
+  addPetToStock(petShop, newPet){
+    /*TEST13*/
+    petShop.pets.push(newPet);
+  },
+  getCustomersCash(customer){
+    /*TEST14*/
+    return customer.cash;
+  },
+  getCustomersCashTotal(customer){
+    /*TEST15*/
+    let total =0;
+    for (var i = 0; i < customer.length; i++) {
+      total+= myFunctions.getCustomersCash(customer[i])
+    };
+    return total;
+  },
+  removeCustomerCash(customer,cash){
+    /*TEST16*/
+    customer.cash -= cash;
+  },
+  getCustomerPetCount(customer){
+    /*TEST17*/
+    return customer.pets.length;
+  },
+  addPetToCustomer(customer, newPet){
+    /*TEST18*/
+    customer.pets.push(newPet);
+  },
+  customerCanAffordPet(customer,newPet){
+    /*TEST19*/ /*TEST20*/
+    if (customer.cash >= newPet.price){
+      return true;
+    };
+  },
+//  sellPetToCustomer(petShop,pet,customer){
+//     /*TEST21*//*TEST22*//*TEST23*/
+//     if petShop has the pet to sell...*/
+//     //if (myFunctions.getPetByName(petShop,pet) != undefined){
+// /*... and the customer can afford the pet */
+//       if (myFunctions.customerCanAffordPet(customer,pet)){
+// /* then removePetByName AND addPetToCustomer */
+//       myFunctions.addPetToCustomer(customer,pet);
+//       myFunctions.removePetByName(petShop,pet);
+// /* then removeCustomerCash AND addOrRemoveCash */
+//       myFunctions.removeCustomerCash(customer,cash);
+//       myFunctions.addOrRemoveCash(petShop,cash);
+// /* then increasePetsSold */
+//       myFunctions.increasePetsSold(petShop,1);
+//       };
+//     };
+//   }
+  sellPetToCustomer(petShop, pet, customer){
+    /*TEST21
+      customer pet count to increase
+      pets sold to increase
+      customer cash to decrease
+      petShop cash to increase */
+    /*TEST22
+      getPetByName needs to return a value */
+    /*TEST23
+      pet.value cannot be more than customer.cash */
+      
+    if(pet){ //returns truthy if pet exists
+      if(customer.cash >= pet.price){
+        myFunctions.removeCustomerCash (customer, pet.price);
+        myFunctions.addOrRemoveCash(petShop, pet.price);
+        myFunctions.addPetToCustomer(customer, pet);
+        myFunctions.increasePetsSold(petShop, 1);
+      };
+    };
+  }
+};
+
+module.exports = myFunctions;
